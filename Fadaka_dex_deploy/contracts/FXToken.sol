951a3eb56200c8c9e0bb4cfd5c2c2b69c6c2b24a
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-04 07:28:09 +0200

    Add files via upload

---
+++ Fadaka_dex_deploy/contracts/FXToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FXToken is ERC20, Ownable {
+    constructor() ERC20("Fadaka Exchange Token", "FX") {
+        _mint(msg.sender, 1_000_000 * 1e18);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyOwner {
+        _burn(from, amount);
+    }
+}
