394fd54f4b9fe9bf241d87bd78a761cfe9d8eb6b
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-02 16:00:31 +0100

    Update and rename . github/ISSUE_TEMPLATE/bash.sh to . github/ISSUE_TEMPLATE.sh

--- . github/ISSUE_TEMPLATE.sh
+++ . github/ISSUE_TEMPLATE.sh
@@ -1,3 +1,4 @@
 https://github.com/QUBUHUB/kubucoinasset/blob/1d5b2a4e1e4037a1e0adab0241bd07b6555e6207/PIA%20VPN-2023-09-04-161226.ips
 My app crashed with termination reason 8BADF00D. Here’s the crash log:  
 https://github.com/QUBUHUB/kubucoinasset/blob/.../PIA%20VPN-2023-09-04-161226.ips
+
